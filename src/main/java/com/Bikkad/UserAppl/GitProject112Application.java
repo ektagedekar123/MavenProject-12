c0b89a5db8c84b252dba3a819e3e29e55ff1e935
ektagedekar123 <gedekarekta123@gmail.com>
2023-01-29 18:11:12 -0800

    Changes by Ekta

---
+++ src/main/java/com/Bikkad/UserAppl/GitProject112Application.java
@@ -0,0 +1,13 @@
+package com.Bikkad.UserAppl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitProject112Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitProject112Application.class, args);
+	}
+
+}
